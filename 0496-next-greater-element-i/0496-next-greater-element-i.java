09d9338b730b1a09a4bdd9e8636ba9c31adab3bf
Aman Rawat <aman17rawat2005@gmail.com>
2026-08-09 14:04:01 +0530

    Time: 3 ms (92.67%), Space: 45 MB (70.52%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,16 +1,15 @@
-class Solution { // Brute force
+class Solution { // Brute force but Optimized using hashmap still TC:- O(n^2) ans SC:- O(n)
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.length];
-        boolean found = false;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums2.length;i++){
+            map.put(nums2[i],i);
+        }
 
         for(int i=0;i<nums1.length;i++){
-            found = false;
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i] == nums2[j]){
-                    found = true;
-                }
+            for(int j=map.get(nums1[i])+1;j<nums2.length;j++){
 
-                if(found == true && nums2[j] > nums1[i]){
+                if(nums2[j] > nums1[i]){
                     ans[i] = nums2[j];
                     break;
                 }
@@ -25,3 +24,32 @@ class Solution { // Brute force
         return ans;
     }
 }
+
+
+// class Solution { // Brute force TC:-O(n^2)
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         int[] ans = new int[nums1.length];
+//         boolean found = false;
+
+//         for(int i=0;i<nums1.length;i++){
+//             found = false;
+//             for(int j=0;j<nums2.length;j++){
+//                 if(nums1[i] == nums2[j]){
+//                     found = true;
+//                 }
+
+//                 if(found == true && nums2[j] > nums1[i]){
+//                     ans[i] = nums2[j];
+//                     break;
+//                 }
+//             }
+//         }
+//         for(int i=0;i<ans.length;i++){
+//             if(ans[i] == 0){
+//                 ans[i] = -1;
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
