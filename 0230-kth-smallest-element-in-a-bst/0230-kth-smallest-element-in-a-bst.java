a465449c306d057ee367f90758d486bcec764ecc
Aman Rawat <aman17rawat2005@gmail.com>
2026-08-13 00:31:48 +0530

    Time: 1 ms (36.47%), Space: 47.1 MB (6.6%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -13,29 +13,61 @@
  *     }
  * }
  */
-class Solution { 
-    // we will use inorder traversal which help to give us sorted elements 
-    int count = 0;
-    int ans = -1;
+class Solution { // most optimize O(n) worst case space is O(h)
+    public int kthSmallest(TreeNode root, int k) { //iterative approach (Moorey)
+        Stack<TreeNode> st = new Stack<>();
 
-    public int kthSmallest(TreeNode root, int k) {
-        inorder(root,k);
+        while(root != null || !st.isEmpty()){ // inorder following
 
-        return ans;
-    }
+            while(root != null){ //  go to the extreme left 1st node smallest 
+                st.push(root);
+                root = root.left;
+            }
 
-    public void inorder(TreeNode root,int k){
-        if(root == null) return;
+            // visit node 
+            root = st.pop();
+            k--;
 
-        inorder(root.left,k);
+            // kth smallest found
+            if(k == 0){
+                return root.val;
+            }
 
-        count++;
+            // move to right subtree
+            root = root.right;
 
-        if(count == k){
-            ans = root.val;
-            return;
         }
 
-        inorder(root.right,k);
+        return -1;
     }
 }
+
+
+
+
+// class Solution { // TC:- O(n) and SC:-O(h)
+//     // we will use inorder traversal which help to give us sorted elements 
+//     int count = 0;
+//     int ans = -1;
+
+//     public int kthSmallest(TreeNode root, int k) {
+//         inorder(root,k);
+
+//         return ans;
+//     }
+
+//     public void inorder(TreeNode root,int k){
+//         if(root == null) return;
+
+//         inorder(root.left,k);
+
+//         count++;
+
+//         if(count == k){
+//             ans = root.val;
+//             return;
+//         }
+
+//         inorder(root.right,k);
+//     }
+// }
