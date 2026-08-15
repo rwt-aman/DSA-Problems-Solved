01cf8553bfe64ffbda9fa31be754ebb64cfea0a0
Aman Rawat <aman17rawat2005@gmail.com>
2026-08-15 12:08:12 +0530

    Time: 6 ms (12.91%), Space: 49.2 MB (5.03%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,14 +1,33 @@
-class Solution { // brute force
+class Solution { // optimize 
     public int[] twoSum(int[] numbers, int target) {
-        int n = numbers.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(numbers[i] + numbers[j] == target){
-                    return new int[]{i+1,j+1};
-                }
+        for(int i=0;i<numbers.length;i++){
+            int rem = target - numbers[i];
+
+            if(map.containsKey(rem)){
+                return new int[]{map.get(rem)+1,i+1};
+            }
+            else{
+                map.put(numbers[i],i);
             }
         }
         return new int[]{-1,-1};
     }
 }
+
+
+// class Solution { // brute force TC:-O(n^2)
+//     public int[] twoSum(int[] numbers, int target) {
+//         int n = numbers.length;
+
+//         for(int i=0;i<n-1;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(numbers[i] + numbers[j] == target){
+//                     return new int[]{i+1,j+1};
+//                 }
+//             }
+//         }
+//         return new int[]{-1,-1};
+//     }
+// }
