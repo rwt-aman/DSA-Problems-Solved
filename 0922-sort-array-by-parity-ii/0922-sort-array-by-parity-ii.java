6e620b2d5dfe93b11ef9a0ac012fcd01343c6547
Aman Rawat <aman17rawat2005@gmail.com>
2026-09-03 21:49:21 +0530

    Time: 3 ms (58.55%), Space: 48.6 MB (47.18%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -1,20 +1,48 @@
-class Solution {
+class Solution { // TC -> O(N) and SC -> O(1)
     public int[] sortArrayByParityII(int[] nums) {
-        int[] temp = new int[nums.length];
         int evenidx = 0;
         int oddidx = 1;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] % 2 == 0){ // even
-                temp[evenidx] = nums[i];
+        while(evenidx < nums.length && oddidx < nums.length){
+            if(nums[evenidx] % 2 == 0){
                 evenidx += 2;
             }
-            else{ // odd
-                temp[oddidx] = nums[i];
+            else if(nums[oddidx] % 2 == 1){
+                oddidx += 2;
+            }
+            else{
+                int temp = nums[evenidx];
+                nums[evenidx] = nums[oddidx];
+                nums[oddidx] = temp;
+
+                evenidx += 2;
                 oddidx += 2;
             }
         }
-        
-        return temp;
+
+        return nums;
     }
 }
+
+
+
+// class Solution { // TC -> O(N) and SC -> O(N)
+//     public int[] sortArrayByParityII(int[] nums) {
+//         int[] temp = new int[nums.length];
+//         int evenidx = 0;
+//         int oddidx = 1;
+
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i] % 2 == 0){ // even
+//                 temp[evenidx] = nums[i];
+//                 evenidx += 2;
+//             }
+//             else{ // odd
+//                 temp[oddidx] = nums[i];
+//                 oddidx += 2;
+//             }
+//         }
+        
+//         return temp;
+//     }
+// }
