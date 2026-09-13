cd158d3b17ced5be99297e71af9a6cd9239a5208
Aman Rawat <aman17rawat2005@gmail.com>
2026-09-13 11:08:33 +0530

    Time: 7 ms (19.4%), Space: 47.3 MB (52.72%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j] == 1){
+                    return dfs(grid,i,j,row,col);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public int dfs(int[][] grid,int i,int j, int row,int col){
+        // Outside grid or water = one boundary
+        if(i < 0 || i >= row || j < 0 || j >= col || grid[i][j] == 0){
+            return 1;
+        }
+
+        if(grid[i][j] == -1){ // already marked
+            return 0; 
+        }
+
+        grid[i][j] = -1; // mark visited
+
+        int count = 0;
+
+        // now calling dfs for left | right | bottom | top
+
+        count += dfs(grid,i+1,j,row,col); // bottom
+        count += dfs(grid,i-1,j,row,col); // top
+        count += dfs(grid,i,j+1,row,col); // right
+        count += dfs(grid,i,j-1,row,col); // left
+
+        return count;
+    }
+}
